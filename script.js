e91d10ffce47490547222d83552f9d75418bae75
Fuzzy Robot <apoorvadubeyand@gmail.com>
2026-05-10 14:54:58 +0000

    New element about added

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+console.log("JavaScript loaded");
+
 async function search() {
+    console.log("Button clicked");
+
     const query = document.getElementById("searchBox").value;
 
-    const res = await fetch(`http://localhost:3000/search?q=${query}`);
+    const res = await fetch(`/search?q=${query}`);
     const data = await res.text();
 
     document.getElementById("result").innerText = data;
