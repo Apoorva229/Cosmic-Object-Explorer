b10155560ba6bf215b53ee03017ce95704bf7d04
Fuzzy Robot <apoorvadubeyand@gmail.com>
2026-05-30 17:54:31 +0000

    FOR more data.

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ const { exec } = require("child_process");
 const app = express();
 
 app.use(express.static(__dirname));
-app.get("/search", (req, res) => {
-    const query = req.query.q;
+app.get("/horizons", async (req, res) => {
+    const url = 
+    'https://ssd.jpl.nasa.gov/api/horizons.api=${GxhGUqbmY6wtf9kORBX8jgv97CLfjxRG9kELbGVY}';
+
+    const response = await fetch(url);
+    const data = await response.json();
+    res.json(data);
+
 
     exec(`./cosmos cosmos.csv "${query}"`, (error, stdout, stderr) => {
         if (error) {
