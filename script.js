326e4f6bd1e19e048185566d00c16f0ed8329e35
Fuzzy Robot <apoorvadubeyand@gmail.com>
2026-06-09 06:52:38 +0000

    Updated database.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 console.log("JavaScript loaded");
 
 async function gethorizon() {
-    const response = await fetch ("horizon");
+    const response = await fetch("horizons");
     const data = await response.json();
     console.log(data);
 
     const query = document.getElementById("searchBox").value;
 
-    const res = await fetch(`/search?q=${query}`);
-    const data = await res.text();
+    const res = await fetch(`/horizons?q=${query}`);
+    const data = await res.json();
 
     document.getElementById("result").innerText = data;
 }
--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const path = require("path");
 const express = require("express");
-const { exec } = require("child_process");
 
 const app = express();
 
 app.use(express.static(__dirname));
+
+const NASA_KEY = "YOUR_KEY_HERE";
+
 app.get("/horizons", async (req, res) => {
-    const url = 
-    'https://ssd.jpl.nasa.gov/api/horizons.api=${GxhGUqbmY6wtf9kORBX8jgv97CLfjxRG9kELbGVY}';
+    const query = req.query.q;
 
-    const response = await fetch(url);
-    const data = await response.json();
-    res.json(data);
+    try {
+        const response = await fetch(
+            `https://ssd.jpl.nasa.gov/api/horizons.api?format=json&COMMAND='${query}'`
+        );
 
+        const data = await response.json();
 
-    exec(`./cosmos cosmos.csv "${query}"`, (error, stdout, stderr) => {
-        if (error) {
-            res.send("Error running program");
-            return;
-        }
-        res.send(stdout);
-    });
+        res.json(data);
+    }
+    catch (error) {
+        res.status(500).send("NASA API ERROR");
+    }
 });
 
 app.listen(3000, () => {
